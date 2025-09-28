91915a1146a924198e603c903339cb37bbebf311
abila <abila@rizenlaptop.lan>
2025-09-28 18:15:30 +0530

    commit

---
+++ corejavaprograms/src/Day3/encapsulation/Encapsulationdemo.java
@@ -0,0 +1,14 @@
+package Day3.encapsulation;
+
+public class Encapsulationdemo {
+
+	public static void main(String[] args) {
+		OoopsConceptDemo obj = new OoopsConceptDemo();
+		obj.setSerialNum(101);
+		obj.setName("Pooja");
+		obj.setAge(20);
+		System.out.println(obj);
+
+	}
+
+}

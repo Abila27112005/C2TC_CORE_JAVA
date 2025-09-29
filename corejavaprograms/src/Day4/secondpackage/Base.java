cddd81457a41b0308ddfc5f3fdb4461b84506654
abila <abila@rizenlaptop.lan>
2025-09-29 22:17:20 +0530

    commit

--- corejavaprograms/src/Day4/secondpackage/Base.java
+++ corejavaprograms/src/Day4/secondpackage/Base.java
@@ -1,37 +1,28 @@
 package Day4.secondpackage;
 
 public class Base {
-	public int varPublic=20;
-	private int varPrivate=30;
-	protected int varProtected=40;
-	
-	/*
-	 * Declaring methods with default, public, private, protected types
-	 */
+    int varDefault = 10;         // default access
+    public int varPublic = 20;   // public access
+    private int varPrivate = 30; // private access
+    protected int varProtected = 40; // protected access
 
-	void methodDefault()
-	{
-		System.out.println("Default access Base class");
-		System.out.println("Default Variable : "+varDefault);
-	}
-	
-	public void methodPublic()
-	{
-		System.out.println("Public access Base class");
-		System.out.println("Public Variable : "+varPublic);
-	}
-	
-	private void methodPrivate()
-	{
-		System.out.println("Private access Base class");
-		System.out.println("Private Variable : "+varPrivate);
-	}
-	
-	protected void methodProtected()
-	{
-		System.out.println("Protected access Base class");
-		System.out.println("Protected Variable : "+varProtected);
-	}
+    void methodDefault() {
+        System.out.println("Default access Base class");
+        System.out.println("Default Variable : " + varDefault);
+    }
 
+    public void methodPublic() {
+        System.out.println("Public access Base class");
+        System.out.println("Public Variable : " + varPublic);
+    }
 
+    private void methodPrivate() {
+        System.out.println("Private access Base class");
+        System.out.println("Private Variable : " + varPrivate);
+    }
+
+    protected void methodProtected() {
+        System.out.println("Protected access Base class");
+        System.out.println("Protected Variable : " + varProtected);
+    }
 }
